626115e22db716cbaf5d13c3d794616f67c418bc
Brenden Seidel <brenseid@gmail.com>
2018-04-23 15:46:17 -0400

    use axios

--- src/App.js
+++ src/App.js
@@ -24,15 +24,14 @@ export default class App extends Component {
       console.log('Error fetching and parsing data', error);
     });
 
-
-    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-    //   .then(response => response.json())
-    //   .then(responseData => {
-    //     this.setState({ gifs: responseData.data })
-    //   })
-    //   .catch(error => {
-    //     console.log('Error fetching and parsing data', error);
-    //   });
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ gifs: responseData.data })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
   }
 
   render() { 
