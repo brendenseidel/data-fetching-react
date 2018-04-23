8e004838184b6252fbb73a854cebaf495c4cf660
Brenden Seidel <brenseid@gmail.com>
2018-04-23 15:58:03 -0400

    add ref and use initial cat search

--- src/App.js
+++ src/App.js
@@ -14,27 +14,29 @@ export default class App extends Component {
   } 
 
   componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-    .then(response => {
-      this.setState({
-        gifs: response.data.data
-      });
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
+    this.performSearch();
 
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data })
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
+    // axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    // .then(response => {
+    //   this.setState({
+    //     gifs: response.data.data
+    //   });
+    // })
+    // .catch(error => {
+    //   console.log('Error fetching and parsing data', error);
+    // });
+
+    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    //   .then(response => response.json())
+    //   .then(responseData => {
+    //     this.setState({ gifs: responseData.data })
+    //   })
+    //   .catch(error => {
+    //     console.log('Error fetching and parsing data', error);
+    //   });
   }
 
-  performSearch = (query) => {
+  performSearch = (query = 'cats') => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
     .then(response => {
       this.setState({
